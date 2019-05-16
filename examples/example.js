0106855ff8ca109cacf3f2e20887845af2d20623
Manish Menaria <the.manish.menaria@gmail.com>
2019-05-16 04:53:35 -0700

    Docs: Updated example code

--- examples/example.js
+++ examples/example.js
@@ -1,5 +1,24 @@
 /* eslint-disable */
 const inputReader = require('../lib/index').inputReader;
 
-let noOfTestCases = inputReader.readInteger();
-console.log('noOfTestCases :', noOfTestCases);
+let noOfCase = inputReader.readInteger();
+console.log(noOfCase,typeof noOfCase);
+while(noOfCase--){
+	let tmp = inputReader.readArray();
+	console.log(tmp, typeof tmp);
+
+	tmp = inputReader.readBoolean();
+	console.log(tmp, typeof tmp);
+
+	tmp = inputReader.readChar();
+	console.log(tmp, typeof tmp);
+
+	tmp = inputReader.readFloat();
+	console.log(tmp, typeof tmp);
+
+	tmp = inputReader.readLine();
+	console.log(tmp, typeof tmp);
+
+	tmp = inputReader.readNumberArray();
+	console.log(tmp, typeof tmp);
+}
--- examples/output.js
+++ examples/output.js
@@ -5,10 +5,23 @@ let inputReader = _inputReader ();
 
 function _main() {
 	
-	_inputLines = __input__.split('\n');
-	
-	let noOfTestCases = inputReader.readInteger();
-	console.log('noOfTestCases :', noOfTestCases);
+	_inputLines = _inputData.split('\n');
+	let noOfCase = inputReader.readInteger();
+	console.log(noOfCase,typeof noOfCase);
+	while(noOfCase--){
+		let tmp = inputReader.readArray();
+		console.log(tmp, typeof tmp);
+		tmp = inputReader.readBoolean();
+		console.log(tmp, typeof tmp);
+		tmp = inputReader.readChar();
+		console.log(tmp, typeof tmp);
+		tmp = inputReader.readFloat();
+		console.log(tmp, typeof tmp);
+		tmp = inputReader.readLine();
+		console.log(tmp, typeof tmp);
+		tmp = inputReader.readNumberArray();
+		console.log(tmp, typeof tmp);
+	}
 
 }
 
@@ -22,12 +35,47 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', cacheInput).on('end', _main);
 
 function _inputReader () {
+	function readArray() {
+		return _inputLines[_lineNumber++].split(' ');
+	}
+		
+	function readBoolean(){
+		let word = _inputLines[_lineNumber++];
+		if(word.toLowerCase() == "true" || word.toLowerCase() == "1"){
+			return true;
+		}else {
+			return true;
+		}
+	}
+		
+	function readChar(){
+		return _inputLines[_lineNumber++].trim();
+	}
+		
+	function readFloat(){
+		return Number(_inputLines[_lineNumber++]);
+	}
+		
 	function readInteger(){
 		return Number(_inputLines[_lineNumber++]);
 	}
 		
+	function readLine(){
+		return _inputLines[_lineNumber++];
+	}
+		
+	function readNumberArray(){
+		return _inputLines[_lineNumber++].split(' ').map(val => Number(val));
+	}
+		
 	
 	return {
+		readArray,
+		readBoolean,
+		readChar,
+		readFloat,
 		readInteger,
+		readLine,
+		readNumberArray,
 	}
 }
