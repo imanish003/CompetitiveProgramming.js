ff156d7d27f8a1b9c3028d5754e71ce4ae494e43
Manish Menaria <the.manish.menaria@gmail.com>
2019-05-16 05:03:07 -0700

    Doc: Add input and output for example

--- examples/example.js
+++ examples/example.js
@@ -22,3 +22,23 @@ while(noOfCase--){
 	tmp = inputReader.readNumberArray();
 	console.log(tmp, typeof tmp);
 }
+
+/* 
+input :
+1
+hajs djahsd jhjhd khakdhjahs 2
+true
+a
+3.23
+This is an awesome line
+1 4 5 8 9 7
+
+Output :
+1 'number'
+[ 'hajs', 'djahsd', 'jhjhd', 'khakdhjahs', '2' ] 'object'
+true 'boolean'
+a string
+3.23 'number'
+This is an awesome line string
+[ 1, 4, 5, 8, 9, 7 ] 'object'
+ */
